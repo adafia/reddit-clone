53c4e4f37b73686e27bce847679616ebc3720f4e
adafia <adafia.samuel@gmail.com>
2020-09-04 17:34:38 +0200

    congigure graphql with express

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
-import microConfig from './mikro-orm.config'
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = await orm.em.create(Post, {title: 'Title of my first post'});
-  // await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
-}
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  apolloServer.applyMiddleware({ app })
+  
+  const PORT = 4000;
+  app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+  });
+};
 
 main();
